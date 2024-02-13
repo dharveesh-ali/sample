148bbb859cea010138f0c0ba7bac318692ef3a37
dharveesh-ali <dharveeshali.n@gmail.com>
2024-02-13 17:10:14 +0530

    task3

---
+++ task3.js
@@ -0,0 +1,10 @@
+const main=document.getElementById('main-heading');
+const header=document.getElementById('header');
+main.textContent='Fruit World';
+header.style.backgroundColor='green';
+main.style.color='orange';
+header.style.borderBottom='3px solid orange';
+const basket=document.getElementById('basket-heading');
+basket.style.color='green';
+const thank=document.getElementById('thanks');
+thank.innerHTML='<p>Please visit us again</p>';
